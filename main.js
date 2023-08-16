bc364aa8da2c3a019cadde2b562aea4d4376575a
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-15 22:13:43 -0700

    add

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+	function key(){
+		
+		sum=0;
+		count=0;
+		
+		for(i=1;i<=10;i++){
+			
+			sum=sum+i;
+			
+			count++;
+		}
+		
+		console.log(sum);
+		avg=sum/count;
+		console.log(avg);
+	}
+	key();
